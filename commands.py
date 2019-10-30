5b0cd631f43e516be5baa4a27abf6deccfff8900
Seraphina Anderson <seraphina.anderson@gmail.com>
2019-10-30 22:03:21 +0000

    Change to print response to .txt file instead

--- commands.py
+++ commands.py
@@ -17,7 +17,6 @@ class Commands():
         return self.coords
 
     def forward(self):
-        # print("testing coords: {}".format(coords))
         y = int(coords["y"]) + 1
         self.coords.update({"y": str(y)})
         return self.coords
@@ -56,64 +55,52 @@ def write_response(file_name, data):
     with open(file_name, 'w+', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
-# test against 1AFC_commands.json
-def gen_initial_response(results):
+def gen_initial_response(results, response_line):
     rover_name = results[0]
     intial_coords = results[1]
     x = intial_coords["x"]
     y = intial_coords["y"]
     deploy = Commands(x, y)
     coords = deploy.set_coords()
-    responses = []
     response = {}
     response.update({"rover-id": rover_name})
     response.update({"position": coords})
     response.update({"direction": "north"})
-    responses.append(response)
-    return responses
+    response_line.write(str(response))
+    response_line.write('\n')
 
-def gen_responses(commands, responses):
+def gen_responses(commands, responses, response_line):
+
     for command in commands:
-        # print("testing responses: {}".format(responses))
         if command == 'move-forward':
-            # print("command in move-forward")
             res = {}
             position = deploy.forward()
             res["rover-id"] = rover_name
             res["position"] = position
             res["direction"] = "north"
-            print(res)
-            forward = res
-            responses.append(forward)
+            response_line.write(str(res))
+            response_line.write('\n')
         elif command == 'turn-left':
-            # print("command in turn-left")
             res = {}
             position = deploy.left()
             res["rover-id"] = rover_name
             res["position"] = position
             res["direction"] = "east"
-            print(res)
-            left = res
-            # print("response: {}".format(res))
-            responses.append(left)
+            response_line.write(str(res))
+            response_line.write('\n')
         elif command == 'turn-right':
-            # print("command in turn-right")
             res = {}
             r_position = deploy.right()
             res["rover-id"] = rover_name
             res["position"] = r_position
             res["direction"] = "west"
-            print(res)
-            right = res
-            # print("response: {}".format(res))
-            responses.append(right)
+            response_line.write(str(res))
+            response_line.write('\n')
         else:
             print("something went wrong!")
-    print("returned responses: {}".format(responses))
+    response_line.close()
     return responses
     
-
-
 def create_json_object(rover_name, responses):
     json_obj = {}
     json_obj = {
@@ -121,24 +108,28 @@ def create_json_object(rover_name, responses):
     }
     return json_obj
 
-
 def write_json_obj(rover_name, responses):
     json_obj = create_json_object(rover_name, responses)
     write_response(str(rover_name) + '_response.json', json_obj)
+
+
+def open_write_file(rover_name):
+    response_line = open(str(rover_name) + '_response.txt', 'a+')
+    return response_line
+
+
 
 
 if __name__ == '__main__':
     # edit initial coordinates
     deploy = Commands(3, 3)
     coords = deploy.set_coords()
-    # edit filename
+    # edit command filename
     results = read_commands('1AFC_commands.json')
     rover_name = results[0]
     initial_position = results[1]
     commands = results[2]
-    responses = gen_initial_response(results)
-    gen_responses(commands, responses)
-    create_json_object(rover_name, responses)
-    write_json_obj(rover_name, responses)
-
+    response_line = open_write_file(rover_name)
+    responses = gen_initial_response(results, response_line)
+    gen_responses(commands, responses, response_line)
     
